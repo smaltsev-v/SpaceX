129bd79c679fba4007b44d2d25e08dcb8fcf1407
Serhii Maltsev <83024347+smaltsev-v@users.noreply.github.com>
2022-07-11 14:19:13 +0300

    add nav
    добавил кнопки навигации

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const FileIncludeWebpackPlugin = require('file-include-webpack-plugin')
+// const HtmlWebpackPartialsInjectorPlugin = require('html-webpack-partials-injector-plugin')
+
 
 const devServer = (isDev) => !isDev ? {} : {
     devServer: {
@@ -9,7 +12,35 @@ const devServer = (isDev) => !isDev ? {} : {
         port: 8080,
     }
 };
-
+// module.exports = {
+//   plugins: [
+//       new HtmlWebpackPartialsInjectorPlugin(), // <-- has to be added once before all html-webpack-plugin(s)
+//       new HtmlWebpackPlugin({
+//           template: './src/pages/layout/layout.ejs',
+//           'title': 'Index-Page',
+//           partials: [{
+//               path: path.resolve('./src/partials/navigation.html'),
+//               location: 'main'
+//           }],
+//           inject: true,
+//           chunks: ['index'],
+//           filename: 'index.html'
+//       }),
+//   ]
+// }
+// module.exports = {
+//   plugins: [
+//     new FileIncludeWebpackPlugin(
+//       {
+//         source: './src/templates', // relative path to your templates
+//         replace: [{
+//           regex: /\[\[FILE_VERSION]]/, // additional things to replace
+//           to: 'v=1.0.0',
+//         }],
+//       },
+//     )
+//   ]
+// }
 module.exports = ({develop}) => ({
   mode: develop ? 'development' : 'production',
   entry: './src/index.js',
